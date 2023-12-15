2d3e499ce696a77447bcc587a91a2167bc6f5d82
Luis Felipe Barbosa <felipebarbosa22@hotmail.com>
2023-12-14 22:39:57 -0300

    Treinando

--- App.js
+++ App.js
@@ -1,23 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
 import { SafeAreaProvider,useSafeAreaInsets } from 'react-native-safe-area-context';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 
 
 function HomeScreen(){
   const insets = useSafeAreaInsets();
   return (
     <View style={{ flex: 1, paddingTop: insets.top, alignItems:'center' }}>
-      <Text style={{ fontSize: 28 }}>Inside safe area</Text>
+      <Text style={{ fontSize: 28 }}>Home Screen</Text>
     </View>
   );
 }
 
+function SettingsScreen(){
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  )
+}
+
+const Tab = createBottomTabNavigator();
+
 export default function App() {
   return (
-    <SafeAreaProvider>
-    <HomeScreen></HomeScreen>
-    </SafeAreaProvider>
-    
+    <NavigationContainer>
+      <Tab.Navigator>
+        
+
+          <Tab.Screen name="Home" component={HomeScreen}></Tab.Screen>
+          <Tab.Screen name="SettingsScreen" component={SettingsScreen}></Tab.Screen>
+
+        
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
